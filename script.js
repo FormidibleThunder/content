0d569e78007d3c00ee0fcfc5db6bc94ca08388f6
FormidibleThunder <shamalperera284@gmail.com>
2025-06-09 17:34:47 +1200

    My first commit using VSC + Github Copilot

---
+++ script.js
@@ -0,0 +1,8 @@
+const email = document.getElementById('emailAddress').value.trim();
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+(!emailPattern.test(email));
+{
+
+}
+
+console.log('Hello World')
